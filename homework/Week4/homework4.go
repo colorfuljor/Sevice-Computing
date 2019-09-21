ccee92c9b59a74a4146642095e46927eed776c68
colorfuljor <913864870@qq.com>
2019-09-21 23:38:18 +0800

    init Week4

---
+++ homework/Week4/homework4.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Value int
+}
+
+// 用于构建结构体切片为最小堆，需要调用down函数
+func Init(nodes []Node) {
+
+}
+
+// 需要down（下沉）的元素在切片中的索引为i，n为heap的长度，将该元素下沉到该元素对应的子树合适的位置，从而满足该子树为最小堆的要求
+func down(nodes []Node, i, n int) {
+
+}
+
+// 用于保证插入新元素(j为元素的索引,切片末尾插入，堆底插入)的结构体切片之后仍然是一个最小堆
+func up(nodes []Node, j int) {
+
+}
+
+// 弹出最小元素，并保证弹出后的结构体切片仍然是一个最小堆，第一个返回值是弹出的节点的信息，第二个参数是Pop操作后得到的新的结构体切片
+func Pop(nodes []Node) (Node, []Node) {
+
+}
+
+// 保证插入新元素时，结构体切片仍然是一个最小堆，需要调用up函数
+func Push(node Node, nodes []Node) []Node {
+
+}
+
+// 移除切片中指定索引的元素，保证移除后结构体切片仍然是一个最小堆
+func Remove(nodes []Node, node Node) []Node {
+
+}
+
+func main() {
+
+}
